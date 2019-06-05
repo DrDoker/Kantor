0b2940d7719eea254667f034d8545e441c328c6e
DrDoker <doker.tk@gmail.com>
2019-06-05 12:20:04 +0200

    PL

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -3,14 +3,12 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,8 +22,7 @@ public class Bot extends TelegramLongPollingBot {
     Currency gpb = new Currency(myRows, 3);
     Currency uah = new Currency(myRows, 11);
 
-    Currency active;
-    Boolean exchange;
+    Boolean exchange, pln;
 
     String textMsg, rez, vvod;
     Double koef;
@@ -39,47 +36,102 @@ public class Bot extends TelegramLongPollingBot {
         if (message != null && message.hasText()) {
             switch (message.getText()) {
                 case "/test":
-                    textMsg = "Привет " + update.getMessage().getChat().getFirstName() + " 😘\n" +
-                            usd.getCurrency() + " " + usd.getBuy() + " " + usd.getSale() + "\n" +
-                            eur.getCurrency() + " " + eur.getBuy() + " " + eur.getSale() + "\n" +
-                            gpb.getCurrency() + " " + gpb.getBuy() + " " + gpb.getSale() + "\n" +
-                            uah.getCurrency() + " " + uah.getBuy() + " " + uah.getSale() + "\n";
+                    textMsg = "Hej " + update.getMessage().getChat().getFirstName()  + ") \uD83D\uDE09\n\n" +
+                            "USD  " + "\uD83C\uDDFA\uD83C\uDDF2" + "  " + usd.getBuy() + "  " + usd.getSale() + "\n" +
+                            "EUR  " + "\uD83C\uDDEA\uD83C\uDDFA"+ "  " + eur.getBuy() + "  " + eur.getSale() + "\n" +
+                            "GPB  " + "\uD83C\uDDEC\uD83C\uDDE7"+ "  " + gpb.getBuy() + "  " + gpb.getSale() + "\n" +
+                            "UAH  " + "\uD83C\uDDFA\uD83C\uDDE6"+ "  " + uah.getBuy() + "  " + uah.getSale() + "\n";
                     sendMsg(message, textMsg);
                     break;
 
-                case "\uD83D\uDCB0 Курс валют \uD83D\uDCB0":
+                case "\uD83D\uDCB0 Kursy walut \uD83D\uDCB0":
                     textMsg =
-                            usd.getCurrency() + " " + usd.getBuy() + " " + usd.getSale() + "\n" +
-                                    eur.getCurrency() + " " + eur.getBuy() + " " + eur.getSale() + "\n" +
-                                    gpb.getCurrency() + " " + gpb.getBuy() + " " + gpb.getSale() + "\n" +
-                                    uah.getCurrency() + " " + uah.getBuy() + " " + uah.getSale() + "\n";
+                            "USD  " + "\uD83C\uDDFA\uD83C\uDDF2" + "  " + usd.getBuy() + "  " + usd.getSale() + "\n" +
+                            "EUR  " + "\uD83C\uDDEA\uD83C\uDDFA"+ "  " + eur.getBuy() + "  " + eur.getSale() + "\n" +
+                            "GPB  " + "\uD83C\uDDEC\uD83C\uDDE7"+ "  " + gpb.getBuy() + "  " + gpb.getSale() + "\n" +
+                            "UAH  " + "\uD83C\uDDFA\uD83C\uDDE6"+ "  " + uah.getBuy() + "  " + uah.getSale() + "\n";
                     sendMsg(message, textMsg);
                     break;
-                case "USD to PLN" :
-
-                    active = usd;
+                case "USD \uD83C\uDDFA\uD83C\uDDF2 -> PLN \uD83C\uDDF5\uD83C\uDDF1":
+                    pln = true;
+                    koef = usd.getBuy();
                     exchange = true;
 
-                    sendMsg(message, "Что хотите сделать?");
+                    sendMsg(message, "Ile chcesz zmienić \uD83C\uDDFA\uD83C\uDDF2 ?");
+                    break;
+                case "PLN \uD83C\uDDF5\uD83C\uDDF1 -> USD \uD83C\uDDFA\uD83C\uDDF2":
+                    pln = false;
+                    koef = usd.getSale();
+                    exchange = true;
+
+                    sendMsg(message, "Ile chcesz zmienić \uD83C\uDDF5\uD83C\uDDF1 ?");
+
+                    break;
+                case "EUR \uD83C\uDDEA\uD83C\uDDFA -> PLN \uD83C\uDDF5\uD83C\uDDF1":
+
+                    pln = true;
+                    koef = eur.getBuy();
+                    exchange = true;
+
+                    sendMsg(message, "Ile chcesz zmienić \uD83C\uDDEA\uD83C\uDDFA ?");
+                    break;
+                case "PLN \uD83C\uDDF5\uD83C\uDDF1 -> EUR \uD83C\uDDEA\uD83C\uDDFA":
+
+                    pln = false;
+                    koef = eur.getSale();
+                    exchange = true;
+
+                    sendMsg(message, "Ile chcesz zmienić \uD83C\uDDF5\uD83C\uDDF1 ?");
+                    break;
+                case "GBP \uD83C\uDDEC\uD83C\uDDE7 -> PLN \uD83C\uDDF5\uD83C\uDDF1":
+                    pln = true;
+                    koef = gpb.getBuy();
+                    exchange = true;
+
+                    sendMsg(message, "Ile chcesz zmienić \uD83C\uDDEC\uD83C\uDDE7 ?");
+
+                    break;
+                case "PLN \uD83C\uDDF5\uD83C\uDDF1 -> GBP \uD83C\uDDEC\uD83C\uDDE7":
+                    pln = false;
+                    koef = gpb.getSale();
+                    exchange = true;
+
+                    sendMsg(message, "Ile chcesz zmienić \uD83C\uDDF5\uD83C\uDDF1 ?");
+
                     break;
 
-                case "Купить" :
-                    koef = active.getSale();
-                    sendMsg(message, "Введите сколько хотите купить " + active.getCurrency() + ")");
+                case "UAH \uD83C\uDDFA\uD83C\uDDE6 -> PLN \uD83C\uDDF5\uD83C\uDDF1":
+                    pln = true;
+                    koef = uah.getBuy();
+                    exchange = true;
+
+                    sendMsg(message, "Ile chcesz zmienić \uD83C\uDDFA\uD83C\uDDE6 ?");
+
                     break;
 
-                case "Продать" :
-                    koef = active.getBuy();
-                    sendMsg(message, "Введите сколько хотите продать " + active.getCurrency() + ")");
+                case "PLN \uD83C\uDDF5\uD83C\uDDF1 -> UAH \uD83C\uDDFA\uD83C\uDDE6":
+
+                    pln = false;
+                    koef = uah.getSale();
+                    exchange = true;
+
+                    sendMsg(message, "Ile chcesz zmienić \uD83C\uDDF5\uD83C\uDDF1 ?");
+
                     break;
 
                 default:
-                    if (exchange == true){
+                    if (exchange == true && pln == true) {
                         vvod = Double.toString(Double.parseDouble(message.getText()));
-                        rez = Double.toString(Double.parseDouble(message.getText()) * koef);
+                        rez = new DecimalFormat("#0.00").format(Double.parseDouble(message.getText()) * koef);
                         exchange = false;
-                        sendMsg(message,  vvod + " = " + rez );
-                    }
+                        sendMsg(message, vvod + " = " + rez + "\uD83C\uDDF5\uD83C\uDDF1");
+                    } else if (exchange == true && pln == false) {
+                        vvod = Double.toString(Double.parseDouble(message.getText()));
+                        rez = new DecimalFormat("#0.00").format(Double.parseDouble(message.getText()) / koef);
+                        exchange = false;
+                        sendMsg(message, vvod + "\uD83C\uDDF5\uD83C\uDDF1" + " = " + rez );
+                    } else sendMsg(message, "Wybierz, co chcesz zrobić !!)");
+
 
             }
         }
@@ -95,14 +147,9 @@ public class Bot extends TelegramLongPollingBot {
         sendMessage.setText(text);
 
         try {
-            if (message.getText().equals("USD to PLN")) {
-                setButton(sendMessage, 1);
-                execute(sendMessage);
-            }else{
-                setButton(sendMessage, 0);
-                execute(sendMessage);
-            }
 
+            setButton(sendMessage);
+            execute(sendMessage);
 
         } catch (TelegramApiException e) {
             e.printStackTrace();
@@ -111,7 +158,7 @@ public class Bot extends TelegramLongPollingBot {
     }
 
 
-    public void setButton(SendMessage sendMessage, int index) {
+    public void setButton(SendMessage sendMessage) {
 
         ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
         sendMessage.setReplyMarkup(replyKeyboardMarkup);
@@ -121,51 +168,34 @@ public class Bot extends TelegramLongPollingBot {
 
         List<KeyboardRow> keyboardRowList = new ArrayList<>();
 
-        if (index == 1) {
-
-            KeyboardRow keyboardRow = new KeyboardRow();
-
-            keyboardRow.add(new KeyboardButton("Купить"));
-            keyboardRow.add(new KeyboardButton("Продать"));
-
-
-            keyboardRowList.add(keyboardRow);
+        KeyboardRow keyboardRow1 = new KeyboardRow();
+        KeyboardRow keyboardRow2 = new KeyboardRow();
+        KeyboardRow keyboardRow3 = new KeyboardRow();
+        KeyboardRow keyboardRow4 = new KeyboardRow();
+        KeyboardRow keyboardRow5 = new KeyboardRow();
 
 
-            replyKeyboardMarkup.setKeyboard(keyboardRowList);
+        keyboardRow1.add(new KeyboardButton("USD \uD83C\uDDFA\uD83C\uDDF2 -> PLN \uD83C\uDDF5\uD83C\uDDF1"));
+        keyboardRow1.add(new KeyboardButton("PLN \uD83C\uDDF5\uD83C\uDDF1 -> USD \uD83C\uDDFA\uD83C\uDDF2"));
 
+        keyboardRow2.add(new KeyboardButton("EUR \uD83C\uDDEA\uD83C\uDDFA -> PLN \uD83C\uDDF5\uD83C\uDDF1"));
+        keyboardRow2.add(new KeyboardButton("PLN \uD83C\uDDF5\uD83C\uDDF1 -> EUR \uD83C\uDDEA\uD83C\uDDFA"));
 
-        } else {
+        keyboardRow3.add(new KeyboardButton("GBP \uD83C\uDDEC\uD83C\uDDE7 -> PLN \uD83C\uDDF5\uD83C\uDDF1"));
+        keyboardRow3.add(new KeyboardButton("PLN \uD83C\uDDF5\uD83C\uDDF1 -> GBP \uD83C\uDDEC\uD83C\uDDE7"));
 
-            KeyboardRow keyboardRow1 = new KeyboardRow();
-            KeyboardRow keyboardRow2 = new KeyboardRow();
-            KeyboardRow keyboardRow3 = new KeyboardRow();
-            KeyboardRow keyboardRow4 = new KeyboardRow();
-            KeyboardRow keyboardRow5 = new KeyboardRow();
+        keyboardRow4.add(new KeyboardButton("UAH \uD83C\uDDFA\uD83C\uDDE6 -> PLN \uD83C\uDDF5\uD83C\uDDF1"));
+        keyboardRow4.add(new KeyboardButton("PLN \uD83C\uDDF5\uD83C\uDDF1 -> UAH \uD83C\uDDFA\uD83C\uDDE6"));
 
+        keyboardRow5.add(new KeyboardButton("\uD83D\uDCB0 Курс валют \uD83D\uDCB0"));
 
-            keyboardRow1.add(new KeyboardButton("USD \uD83C\uDDFA\uD83C\uDDF2 to PLN \uD83C\uDDF5\uD83C\uDDF1"));
-            keyboardRow1.add(new KeyboardButton("PLN \uD83C\uDDF5\uD83C\uDDF1 to USD \uD83C\uDDFA\uD83C\uDDF2"));
+        keyboardRowList.add(keyboardRow1);
+        keyboardRowList.add(keyboardRow2);
+        keyboardRowList.add(keyboardRow3);
+        keyboardRowList.add(keyboardRow4);
+        keyboardRowList.add(keyboardRow5);
 
-            keyboardRow2.add(new KeyboardButton("EUR \uD83C\uDDEA\uD83C\uDDFA to PLN \uD83C\uDDF5\uD83C\uDDF1"));
-            keyboardRow2.add(new KeyboardButton("PLN \uD83C\uDDF5\uD83C\uDDF1 to EUR \uD83C\uDDEA\uD83C\uDDFA"));
-
-            keyboardRow3.add(new KeyboardButton("GBP \uD83C\uDDEC\uD83C\uDDE7 to PLN \uD83C\uDDF5\uD83C\uDDF1"));
-            keyboardRow3.add(new KeyboardButton("PLN \uD83C\uDDF5\uD83C\uDDF1 to GBP \uD83C\uDDEC\uD83C\uDDE7"));
-
-            keyboardRow4.add(new KeyboardButton("UAH \uD83C\uDDFA\uD83C\uDDE6 to PLN \uD83C\uDDF5\uD83C\uDDF1"));
-            keyboardRow4.add(new KeyboardButton("PLN \uD83C\uDDF5\uD83C\uDDF1 to UAH \uD83C\uDDFA\uD83C\uDDE6"));
-
-            keyboardRow5.add(new KeyboardButton("\uD83D\uDCB0 Курс валют \uD83D\uDCB0"));
-
-            keyboardRowList.add(keyboardRow1);
-            keyboardRowList.add(keyboardRow2);
-            keyboardRowList.add(keyboardRow3);
-            keyboardRowList.add(keyboardRow4);
-            keyboardRowList.add(keyboardRow5);
-
-            replyKeyboardMarkup.setKeyboard(keyboardRowList);
-        }
+        replyKeyboardMarkup.setKeyboard(keyboardRowList);
 
 
     }
